f9ff38612d83ccb6e1d77c90ddc5d96b2a027917
Andrew Sagatelov <>
2023-06-09 00:39:26 +0300

    added alerts clouger

--- Alerts and Git/Alerts and Git/ViewController.swift
+++ Alerts and Git/Alerts and Git/ViewController.swift
@@ -14,10 +14,24 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    enum filmType: String {
+        case film1 = "Spider man"
+        case film2 = "Venom"
+        
+    }
+    
+    let handler: (_ filmType: filmType) -> (UIAlertAction) -> Void = {
+        filmType in
+        return {
+            action in
+            print(filmType.rawValue)
+        }
+        
+    }
     @IBAction func alertAction(_ sender: Any) {
         let alert = UIAlertController(title: "Photo access", message: "Your photo is not the good pass", preferredStyle: .alert)
-        let alertAction = UIAlertAction(title: "ok", style: .default)
-        let alertActionCancel = UIAlertAction(title: "cancel", style: .cancel)
+        let alertAction = UIAlertAction(title: "ok", style: .default, handler: handler(.film1))
+        let alertActionCancel = UIAlertAction(title: "cancel", style: .cancel, handler: handler(.film2))
         
         alert.addAction(alertAction)
         alert.addAction(alertActionCancel)

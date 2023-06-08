5819ce76625eac099a88dd4a327c3436b55124df
Andrew Sagatelov <>
2023-06-08 22:48:04 +0300

    added alert

--- Alerts and Git/Alerts and Git/ViewController.swift
+++ Alerts and Git/Alerts and Git/ViewController.swift
@@ -14,6 +14,33 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func alertAction(_ sender: Any) {
+        let alert = UIAlertController(title: "Photo access", message: "Your photo is not the good pass", preferredStyle: .alert)
+        let alertAction = UIAlertAction(title: "ok", style: .default)
+        let alertActionCancel = UIAlertAction(title: "cancel", style: .cancel)
+        
+        alert.addAction(alertAction)
+        alert.addAction(alertActionCancel)
+        
+        present(alert, animated: true)
+        
+    }
+    
+   
+    
+    @IBAction func sheetAction(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Access", message: "you password is not alow", preferredStyle: .actionSheet)
+        
+        let alertAction = UIAlertAction(title: "ok", style: .default)
+        let alertActionCancel = UIAlertAction(title: "cancel", style: .cancel)
+        
+        alert.addAction(alertAction)
+        alert.addAction(alertActionCancel)
+        
+        present(alert, animated: true)
+    }
+    
+    
 }
 
